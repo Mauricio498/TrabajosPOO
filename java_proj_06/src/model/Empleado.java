ce75dd5dd15147d530fb80bc94caa685c31b9930
Mauricio498 <perezhernandezmauricio225@gmail.com>
2026-06-10 13:24:14 -0600

    Se añade nuevo proyecto

---
+++ java_proj_06/src/model/Empleado.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Empleado {
+    private String nombre;
+    private int edad;
+    private double sueldo;
+
+    //Constructor
+    public Empleado(String nombre, int edad, double sueldo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado [nombre=" + nombre + ", edad=" + edad + ", sueldo=" + sueldo + "]";
+    }
+    
+}
